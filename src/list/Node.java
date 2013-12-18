ea4b02f1af590eafd9695a7c22dcd6907d88ef34
MisterTu <j.johnster@gmx.de>
2013-12-18 23:44:01 +0100

    unfinished getLastNode and reversedList

--- src/list/Node.java
+++ src/list/Node.java
@@ -3,7 +3,8 @@ package list;
 public class Node {
 
 		String data;
-		Node next;
+		Node next = null;
+		
 	public Node(String string) {
 		data =string;
 	}
@@ -16,6 +17,7 @@ public class Node {
 	public Node add(String string) {
 		return new Node(string,this);
 	}
+	
 	@Override
 	public String toString(){
 		if (next == null)
@@ -23,5 +25,102 @@ public class Node {
 		else
 		  return data + ","+next.toString();
 	}
+	
+	public boolean hasNext(){
+		return next != null;
+	}
+	
+	public Node getNext(){
+		
+		return next;
+	}
+	
+	public Node getLast(){
+		Node last = this;
+		
+		while(last.hasNext()){
+			last = last.next;
+		}
+		
+		return last;
+	}
+	
+	public void append(Node lastNode){
+		Node last = getLast();
+		
+		last.next = lastNode;
+	}
+	
+	public int getLength(){
+		Node last = this;
+		int i = 1;
+		
+		while(last.hasNext()){
+			last = last.next;
+			i++;
+		}
+		
+		return i;
+	}
+	
+	
+	//if position > list-lenght = delete last node
+	public void delete(int position){
+		Node antecessor = this;
+		
+		if(position != 0){
+			
+			if(position > getLength()-1){
+				position = getLength()-1;
+			}
+			
+			for(int i=0; i<position-1; i++){
+				antecessor = antecessor.next;
+			}
+			
+			antecessor.next = antecessor.next.next;
+		}
+		else if(position == 0){
+			Node tempo = antecessor;
+			antecessor = antecessor.next;
+			tempo.next = null;
+		}
+		
+		
+	}
+	
+	public Node getNodeInPos(int position){
+		Node antecessor = this;
+		
+		for(int i=0; i<position-1; i++){
+			antecessor = antecessor.next;
+			System.out.println("Position: " + position);
+			System.out.println("i: " + i);
+		}
+		
+		
+		return antecessor;
+	}
+	
+//	public Node reverse(){
+//		int nextToReverse = getLength();
+//		Node reversedList = getNodeInPos(nextToReverse);
+//		
+//		nextToReverse --;
+//		
+//		for(int i = nextToReverse; i >= 0; i--){
+//			
+//			
+//			
+//			reversedList.append(getNodeInPos(nextToReverse));
+//			
+//			System.out.println("nextToReverse: " + nextToReverse);
+//			
+//		}
+//		
+//		return reversedList;
+//	}
+	
+	
 
 }
--- test/list/NodeTest.java
+++ test/list/NodeTest.java
@@ -35,5 +35,54 @@ public class NodeTest {
 		// list should be a,b
 		assertEquals("bla","a,b",result.toString());
 	}
+	
+	@Test
+	public void testAppend(){
+		Node list = new Node("Y",new Node("M", new Node("C")));
+		list.append(new Node("A"));
+		
+		assertEquals("Y,M,C,A",list.toString());
+		
+	}
+	
+	@Test
+	public void testDelete(){
+		Node list = new Node("q",new Node("u", new Node("e", new Node("f", new Node("e", new Node("n"))))));
+		list.delete(3);
+		
+		assertEquals("q,u,e,e,n",list.toString());
+	}
+	
+//	@Test
+//	public void testDeleteFirstNode(){
+//		Node list = new Node("f",new Node("r", new Node("a", new Node("i", new Node("l", new Node("s"))))));
+//		list.delete(0);
+//		
+//		assertEquals("r,a,i,l,s",list.toString());
+//	}
+	
+	@Test
+	public void testDeleteLastNode(){
+		Node list = new Node("f",new Node("r", new Node("a", new Node("i", new Node("l", new Node("s"))))));
+		list.delete(2000);
+		
+		assertEquals("f,r,a,i,l",list.toString());
+	}
+	
+//	@Test
+//	public void testReverseList(){
+//		Node list = new Node("r",new Node("a", new Node("i", new Node("l"))));
+//		list = list.reverse();
+//		
+//		assertEquals("l,i,a,r",list.toString());
+//	}
+	
+	@Test
+	public void testNodeInPos(){
+		Node list = new Node("f",new Node("r", new Node("a", new Node("i", new Node("l", new Node("s"))))));
+		list = list.getNodeInPos(3);
+		
+		assertEquals("i",list.toString());
+	}
 
 }
